428ead69e94d79720f36246183aaadd1495cb66a
Apprentice <apprentice@devbootcamp.com>
2012-11-12 15:53:10 -0800

    got lists displaying and create form setup

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,19 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '53c191f09496c79364261b88d8470ff7c6fe3433039bb1598a35fbcaf678671b33211d2d46afcab7ebc3c58aa8d0a0392bfc4222e25159e7b055fc7cd5c01908'
